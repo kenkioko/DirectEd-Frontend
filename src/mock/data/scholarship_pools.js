dc50dd7461bc2f43ce9b8a84e7feb0270b71ac75
Kioko Kennedy <kmkioko@gmail.com>
2022-10-05 02:21:42 +0300

    Mock Data
    Mock data for the donation pools

---
+++ src/mock/data/scholarship_pools.js
@@ -0,0 +1,29 @@
+import st_peters_img from '../images/St. Peters.png';
+import strathmore_img from '../images/Strathmore.png';
+
+export const donations = [
+    {
+        id: 0,
+        image: st_peters_img,
+        title: "St. Peters High School",
+        read_more_url: "https://directed.dev/",
+        scholarship_funded: 23,
+        remaining_rights: 1,
+        days_left: 33,
+        people_donated: 124,
+        fund_balance: 300,
+        percent_donated: 60
+    },
+    {
+        id: 1,
+        title: "Strathmore High School",
+        image: strathmore_img,
+        read_more_url: "https://directed.dev/",
+        scholarship_funded: 40,
+        remaining_rights: 2,
+        days_left: 12,
+        people_donated: 194,
+        fund_balance: 100,
+        percent_donated: 80
+    }
+];
