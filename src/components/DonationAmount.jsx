45c1e959be9f01595858a222e90b6745d87fa343
Kioko Kennedy <kmkioko@gmail.com>
2022-10-07 05:55:30 +0300

    Select Amount
    Donate button active after selecting an amount

--- src/components/DonationAmount.jsx
+++ src/components/DonationAmount.jsx
@@ -1,31 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 
-function DonationAmount(props) {
-    const amounts = [1000, 500, 100, "custom"];
+class DonationAmount extends Component {
 
-    return (
-        <Container className="my-5 py-3">
-            <div className="text-center mt-5">
-                <h4 className="text-capitalize">Select Donation Amount</h4>
-            </div>
+    constructor(props) {
+        super(props);
+        this.state = { amount: null };
+    }
 
-            <div className="d-flex justify-content-center py-5">
-                {amounts.map((amount, idx) => (
-                    <Button key={idx} variant="secondary" size="lg" className="mx-2 w-10">
-                        {amount !== 'custom' && '₳'}{amount}
+    setAmount = (amount, e) => {
+        this.setState({ amount: amount });
+    };
+
+    render() {
+        const amounts = [1000, 500, 100, "custom"];
+
+        return (
+            <Container className="my-5 py-3">
+                <div className="text-center mt-5">
+                    <h4 className="text-capitalize">Select Donation Amount</h4>
+                </div>
+
+                <div className="d-flex justify-content-center py-5">
+                    {amounts.map((amount, idx) => (
+                        <Button
+                            key={idx}
+                            variant={this.state.amount === amount ? "primary" : "secondary"}
+                            size="lg"
+                            className="mx-2 w-10"
+                            onClick={(e) => this.setAmount(amount, e)}
+                        >
+                            {amount !== 'custom' && '₳'}{amount}
+                        </Button>
+                    ))}
+                </div>
+
+                <div className="d-flex justify-content-center">
+                    <Button
+                        variant="primary"
+                        href="/confirmation"
+                        className="w-15"
+                        disabled={this.state.amount > 0 ? false : true}
+                    >
+                        Donate
                     </Button>
-                ))}
-            </div>
+                </div>
+            </Container>
+        );
+    }
 
-            <div className="d-flex justify-content-center">
-                <Button variant="primary" className="w-15" href="/confirmation">
-                    Donate
-                </Button>
-            </div>
-        </Container>
-    )
 }
 
 export default DonationAmount;
