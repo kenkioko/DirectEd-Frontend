c0c44b05e26382e96c8653f8194e8d467651b478
Kioko Kennedy <kmkioko@gmail.com>
2022-10-07 03:44:00 +0300

    Scholar's Progress Mock Data

---
+++ src/mock/data/scholars_progress.js
@@ -0,0 +1,38 @@
+import akinyi_zawadi_img from '../images/scholars_progress/akinyi_zawadi.png';
+import nekesa_makena_img from '../images/scholars_progress/nekesa_makena.png';
+import keruba_rehema_img from '../images/scholars_progress/keruba_rehema.png';
+import wawuda_wawira_img from '../images/scholars_progress/wawuda_wawira.png';
+import chepkirui_makena_img from '../images/scholars_progress/chepkirui_makena.png';
+
+export const progress = [
+    {
+        id: 0,
+        image: akinyi_zawadi_img,
+        name: 'Akinyi Zawadi',
+        progress: 3,
+    },
+    {
+        id: 1,
+        image: nekesa_makena_img,
+        name: 'Nekesa Makena',
+        progress: 1,
+    },
+    {
+        id: 2,
+        image: keruba_rehema_img,
+        name: 'Keruba Rehema',
+        progress: 2,
+    },
+    {
+        id: 3,
+        image: wawuda_wawira_img,
+        name: 'Wawuda Wawira',
+        progress: 3,
+    },
+    {
+        id: 4,
+        image: chepkirui_makena_img,
+        name: 'Chepkirui Makena',
+        progress: 4,
+    },
+];
